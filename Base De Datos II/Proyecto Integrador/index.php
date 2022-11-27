a09c0eb05dc3ade3a905ef154d6bb039608e04ed
Gobre <a19300100@live.ceti.mx>
2022-11-27 15:55:44 -0600

    new file:   Base De Datos II/Proyecto Integrador/db.php new file:   Base De Datos II/Proyecto Integrador/home.php new file:   Base De Datos II/Proyecto Integrador/index.php new file:   Base De Datos II/Proyecto Integrador/validar.php

---
+++ Base De Datos II/Proyecto Integrador/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="validar.php" method="post">
+        <h1>Sistema de Login</h1>
+        <p>Nombre <input type="text" placeholder="Ingrese su nombre" name="usuario"></p>
+        <p>Codigo de empleado <input type="password" placeholder="Ingrese su numero de empleado" name="contraseña"></p>
+        <input type="submit" value="Ingresar">
+</body>
+
+</html>
