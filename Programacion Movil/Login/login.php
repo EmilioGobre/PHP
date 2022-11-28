fe38396c126d71be68a84d0a501943632c4d2450
Gobre <a19300100@live.ceti.mx>
2022-11-27 20:48:43 -0600

    login

---
+++ Programacion Movil/Login/login.php
@@ -0,0 +1,19 @@
+<?php
+
+$conexion = mysqli_connect('localhost', 'root', '', 'mymovie');
+
+if (!$conexion) {
+    echo 'Error al conectar a la base de datos';
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$query="SELECT * FROM usuario WHERE email='$email' AND password=MD5('$password')";
+$resultado = mysqli_query($conexion, $query);
+
+if (mysqli_num_rows($resultado) > 0) {
+    echo "Login exitoso";
+} else {
+    echo "Error al iniciar sesión";
+}
