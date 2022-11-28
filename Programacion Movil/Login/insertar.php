5d69a4bd7e35b2379e3bbb7806863b541ad10063
Gobre <a19300100@live.ceti.mx>
2022-11-27 20:34:10 -0600

    insertar

--- Programacion Movil/Login/insertar.php
+++ Programacion Movil/Login/insertar.php
@@ -1,5 +1,20 @@
 <?php
 
-$conexion = mysqli_connect('localhost','root','','mymovie') or die("No se pudo conectar a la base de datos");
+$conexion = mysqli_connect('localhost', 'root', '', 'mymovie');
 
-if
+if (!$conexion) {
+    echo 'Error al conectar a la base de datos';
+}
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$query = "INSERT INTO usuario (nombre, email, password) VALUES ('$nombre', '$email', MD5('$password'))";
+$resultado = mysqli_query($conexion, $query);
+
+if ($resultado) {
+    echo "Registro exitoso";
+} else {
+    echo "Error al registrar";
+}
